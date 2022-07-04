4b7cdddb6068fbcf08d049f84db9e339368e1ab7
Thang <vohuuthang9595@gmail.com>
2022-07-04 11:26:33 +0700

    Updated

--- index.js
+++ index.js
@@ -47,12 +47,26 @@ app.use((req, res, next) => {
   next();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found!");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
   });
-}
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
